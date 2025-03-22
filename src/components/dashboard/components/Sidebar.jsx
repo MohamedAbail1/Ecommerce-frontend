b4e32ea0ae17b54f63fa253688978908648f580b
MedDev <mohamedabail414@gmail.com>
2025-03-22 14:24:10 +0000

    add readme file

--- src/components/dashboard/components/Sidebar.jsx
+++ src/components/dashboard/components/Sidebar.jsx
@@ -1,8 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUsers, faShoppingBag, faBoxOpen, faClipboardList } from '@fortawesome/free-solid-svg-icons';
+import { faHome,faCog, faUsers, faShoppingBag, faBoxOpen, faClipboardList, faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
+  const handleLogout = () => {
+    // Logique de déconnexion (par exemple, supprimer le token, rediriger vers la page de login, etc.)
+    console.log('Logging out...');
+  };
+
+  const handleProfile = () => {
+    // Logique pour afficher la page de profil
+    console.log('Navigating to profile...');
+  };
+
   return (
     <div className="w-64 bg-gray-800 text-white p-4">
       <nav className="space-y-4">
@@ -26,6 +36,23 @@ export default function Sidebar() {
           <FontAwesomeIcon icon={faClipboardList} className="h-5 w-5 mr-3" />
           Orders
         </Link>
+
+        <p className='flex items-center'>ACCOUNT PAGES</p>
+        {/* Section Profil */}
+        <div className="flex items-center hover:bg-gray-700 p-2 rounded cursor-pointer" onClick={handleProfile}>
+          <FontAwesomeIcon icon={faUser} className="h-5 w-5 mr-3" />
+          Profile
+        </div>
+        {/* Section Profil */}
+        <div className="flex items-center hover:bg-gray-700 p-2 rounded cursor-pointer" onClick={handleProfile}>
+          <FontAwesomeIcon icon={faCog} className="h-5 w-5 mr-3" />
+          Profile
+        </div>
+        {/* Section Log Out */}
+        <div className="flex items-center hover:bg-gray-700 p-2 rounded cursor-pointer" onClick={handleLogout}>
+          <FontAwesomeIcon icon={faSignOutAlt} className="h-5 w-5 mr-3" />
+          Log Out
+        </div>
       </nav>
     </div>
   );
