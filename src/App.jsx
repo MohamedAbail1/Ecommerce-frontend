e5143a50c4217f478afdca3b555d1577527c76d4
MedDev <mohamedabail414@gmail.com>
2025-03-22 22:17:28 +0000

    add admin authentication(+Login)

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,37 @@ import Users from "./components/dashboard/pages/Users";
 import Products from "./components/dashboard/pages/Products";
 import Categories from "./components/dashboard/pages/Categories";
 import Orders from "./components/dashboard/pages/Orders";
+import Login from "./components/dashboard/pages/Login";
 
 function App() {
   return (
     <Router>
-      <div className="flex">
-        <Sidebar />
-        <div className=" p-6 w-full bg-gray-100 min-h-screen">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/orders" element={<Orders />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        {/* Route pour /login sans Sidebar */}
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        {/* Routes avec Sidebar */}
+        <Route
+          path="/*"
+          element={
+            <div className="flex">
+              <Sidebar />
+              <div className="p-6 w-full bg-gray-100 min-h-screen">
+                <Routes>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/users" element={<Users />} />
+                  <Route path="/products" element={<Products />} />
+                  <Route path="/categories" element={<Categories />} />
+                  <Route path="/orders" element={<Orders />} />
+                </Routes>
+              </div>
+            </div>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
