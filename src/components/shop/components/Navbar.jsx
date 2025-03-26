a5969e70162275eaa206be06057697d01bbf2a26
MedDev <mohamedabail414@gmail.com>
2025-03-26 21:55:20 +0000

    amend path

--- src/components/shop/components/Navbar.jsx
+++ src/components/shop/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-white shadow-md p-4 flex justify-between items-center">
-      <Link to="/shop" className="text-xl font-bold text-blue-600">
+      <Link to="/" className="text-xl font-bold text-blue-600">
         MyShop
       </Link>
       <div className="space-x-4">
-        <Link to="/shop" className="text-gray-700 hover:text-blue-500">
+        <Link to="/" className="text-gray-700 hover:text-blue-500">
           Home
         </Link>
         <Link to="/shop/cart" className="text-gray-700 hover:text-blue-500">
